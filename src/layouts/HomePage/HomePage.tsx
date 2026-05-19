292291a331fb4bbf7a59302f7efad144fc018acb
devjosfe <devjosfe@gmail.com>
2026-05-19 02:02:05 -0700

    Fixed some issues

---
+++ src/layouts/HomePage/HomePage.tsx
@@ -0,0 +1,17 @@
+import { Footer } from "../NavbarAndFooter/Footer";
+import { Navbar } from "../NavbarAndFooter/Navbar";
+import { Carousel } from "./Components/Carousel";
+import { ExploreTopBooks } from "./Components/ExploreTopBooks";
+import { Heros } from "./Components/Heros";
+import { LibraryServices } from "./Components/LibraryServices";
+
+export const HomePage = () => {
+  return (
+    <>
+      <ExploreTopBooks />
+      <Carousel />
+      <Heros />
+      <LibraryServices />
+    </>
+  );
+};
